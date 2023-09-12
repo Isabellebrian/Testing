07cdab07410dfbb1c685c7a3b2a202cad3010cbd
Isabellebrian <Isabellebrian@gmail.com>
2023-09-12 20:22:52 +1000

    Initial Commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Isabelle Brian on 12/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
